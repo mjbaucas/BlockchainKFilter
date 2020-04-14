e9c1140cfcd8d830bc23564f4e701d0a0e4d0f6e
mjbaucas <mjbaucas@gmail.com>
2020-04-14 04:25:13 -0400

    added code for RSSI filter

--- filters/kalman.py
+++ filters/kalman.py
@@ -1,7 +1,29 @@
 from numpy import dot, eye, add, subtract, nan_to_num
 from numpy.linalg import inv, pinv
+from math import isnan, isinf
 
-class KalmanFilter(object):
+def check_val(x):
+	if isnan(x):
+		return 0.0
+	
+	if isinf(x):
+		if x >= 0:
+			return 1.7976931348623157e+308
+		else:
+			return -1.7976931348623157e+308
+
+	return x
+
+def inv_div(num, den):
+	if den == 0:
+		if num >= 0:
+			return 1.7976931348623157e+308
+		else:
+			return -1.7976931348623157e+308
+
+	return check_val(num/den)
+
+class KalmanFilter2DPlus(object):
 	def _predict(self, X, U, P, A, B, Q):
 		X = nan_to_num(dot(A, X)) + nan_to_num(dot(B, U))
 		P = nan_to_num(dot(A, nan_to_num(dot(P, A.T)))) + Q 		
@@ -20,3 +42,20 @@ class KalmanFilter(object):
 		return new_X, new_P
 
 
+class KalmanFilter1D(object):
+	def _predict(self, X, U, P, A, B, Q):
+		X = check_val(A*X) + check_val(B*U)
+		P = check_val(A*P*A) + Q
+		return X, P
+
+	def _update(self, X, Z, P, C, R):
+		K = inv_div(check_val(P*C),(check_val(C*P*C) + R))
+		X = X + check_val(K*(Z - check_val(C*X)))
+		P = check_val((1-check_val(K*C))*P*(1-check_val(K*C))) + check_val(K*R*K)
+		return X, P
+
+	def filter(self, X, Z, U, P, A, B, C, Q, R):	
+		predicted_X, predicted_P = self._predict(X, U, P, A, B, Q)
+		new_X, new_P = self._update(predicted_X, Z, predicted_P, C, R)
+
+		return new_X, new_P
---
+++ main.py
@@ -0,0 +1,48 @@
+from numpy import array, eye, diag, zeros, arange, ndarray, dot, empty, append, nan_to_num
+from numpy.linalg import norm
+from numpy.random import multivariate_normal, randn
+
+from math import sqrt
+import matplotlib.pyplot as plt
+
+from filters.kalman import KalmanFilter1D
+
+if __name__ == "__main__":
+    T = 1e-3
+    t = list(arange(0, 1, T))
+    A = 1
+    B = 0
+    C = 1
+    Q = 1e-3
+    R = 1e-3
+
+    X = [0]*len(t)
+    Z = randn(len(t))
+    for i in range(len(Z)):
+        if i >= len(t)/8:
+            Z[i]+=67
+        if i >= (len(t)/8)*2:
+            Z[i]-=67
+        if i >= len(t)/2:
+            Z[i]+=120
+        if i >= (len(t)/4)*3:
+            Z[i]-=100
+
+    U = randn(len(t))
+    P_KF = [0]*len(t)
+    X_KF = [0]*len(t)
+
+    X[0] = None
+    P_KF[0] = Q
+    A_KF = 1
+
+    kf = KalmanFilter1D()
+    for k in range(1, len(t)-1):
+        if X[k] == None:
+            X_KF[k+1] = Z[k]*(1/C)
+        else:
+            X_KF[k+1], P_KF[k+1] = kf.filter(X_KF[k], Z[k], U[k], P_KF[k], A_KF, B, C, Q, R)
+
+    plt.plot(t, Z[:])
+    plt.plot(t, X_KF[:])
+    plt.savefig('test4.png')
