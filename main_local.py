00e51d3e091e31a48dbf9aea1b0861fa7189fff0
mjbaucas <mjbaucas@gmail.com>
2022-11-25 02:24:21 -0500

    Tuning

--- main_local.py
+++ main_local.py
@@ -53,10 +53,10 @@ if __name__ == "__main__":
         rssi = get_RSSI(5)
         if rssi != 0:
             Z.append(rssi)
-            D.append(get_distance(rssi, C=-49, N=2))   
+            D.append(get_distance(rssi, C=-45, N=2))   
             if k == 0:
                 X_KF.append(Z[k]*(1/C))
-                D_KF.append(get_distance(X_KF[k], C=-49, N=2))  
+                D_KF.append(get_distance(X_KF[k], C=-45, N=2))  
                 k+=1
             else:
                 new_X_KF, new_P_KF = kf.filter(X_KF[k-1], Z[k], 0, P_KF[k-1], A, B, C, Q, R)
@@ -79,7 +79,7 @@ if __name__ == "__main__":
                     message = received.split("_")
                     X_KF.append(float(message[1]))
                     P_KF.append(float(message[0])/Beta)
-                    D_KF.append(get_distance(X_KF[k], C=-49, N=2))  
+                    D_KF.append(get_distance(X_KF[k], C=-45, N=2))  
             
             if global_recv == 1:
                 # Debugging prints
