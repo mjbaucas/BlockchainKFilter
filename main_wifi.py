a60effc0771cafd494d98adee546d9e00ac46e6f
mjbaucas <mjbaucas@gmail.com>
2023-03-07 02:17:00 -0500

    Changes in distance formula

--- main_wifi.py
+++ main_wifi.py
@@ -48,14 +48,14 @@ if __name__ == "__main__":
         rssi = get_RSSI(5)
         if rssi != 0:
             Z.append(rssi)
-            D.append(get_distance(rssi, C=-57, N=2))   
+            D.append(get_distance(rssi, C=-55, N=2))   
             if k == 0:
                 X_KF.append(Z[k]*(1/C))
             else:
                 new_X_KF, new_P_KF = kf.filter(X_KF[k-1], Z[k], 0, P_KF[k-1], A, B, C, Q, R)
                 X_KF.append(new_X_KF)
                 P_KF.append(new_P_KF)
-            D_KF.append(get_distance(X_KF[k], C=-57, N=2))  
+            D_KF.append(get_distance(X_KF[k], C=-55, N=2))  
             
             # Debugging prints
             print(D[k])
